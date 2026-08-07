49c183a502d30e2be77f786846018b07cbee25cc
Leuzimar <74550000+Leuzimar@users.noreply.github.com>
2026-08-07 20:39:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,64 @@
-const CACHE = "lbs3d-v2";
+const CACHE = "lbs3d-corrigido-v2";
 
 const FILES = [
-  "/LBS-3D/",
-  "/LBS-3D/index.html",
-  "/LBS-3D/manifest.json"
+"./",
+"./index.html",
+"./manifest.json",
+"./logo.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(FILES))
-      .then(() => self.skipWaiting())
-  );
+
+event.waitUntil(
+
+```
+caches.open(CACHE)
+  .then(cache => cache.addAll(FILES))
+```
+
+);
+
+self.skipWaiting();
+
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
+
+event.waitUntil(
+
+```
+caches.keys().then(keys =>
+
+  Promise.all(
+
+    keys
+      .filter(key => key !== CACHE)
+      .map(key => caches.delete(key))
+
+  )
+
+)
+```
+
+);
+
+self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
 
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, copy);
-        });
+event.respondWith(
 
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+```
+caches.match(event.request)
+  .then(response => {
+
+    return response || fetch(event.request);
+
+  })
+```
+
+);
+
 });
